6dc335bbcceb4aad2cde788e2bd008513f330499
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-12-18 17:11:10 +0530

    🔊 Add log level for api in .env

--- api/_logger.py
+++ api/_logger.py
@@ -1,11 +1,25 @@
 import functools
 import logging
+import os
 from datetime import datetime as dt
 from pathlib import Path
 
+import dotenv
+
+
+@functools.cache
+def __get_log_level() -> int:
+    dotenv.load_dotenv()  # Load .env file explicitly once
+    level = os.getenv("API_LOG_LEVEL")
+    if level is None:
+        raise ValueError("Provide 'API_LOG_LEVEL' variable in '.env' file.")
+    if level in logging._nameToLevel:
+        return getattr(logging, level)
+    raise ValueError(f"{level!r} is not valid log level.")
+
 
 def __configure_logger(logger: logging.Logger, fp: Path):
-    logger.setLevel(logging.INFO)
+    logger.setLevel(__get_log_level())
     fp.parent.mkdir(parents=True, exist_ok=True)
     file_handler = logging.FileHandler(fp)
     formatter = logging.Formatter(
