011aed440d0753900863141b98a7f316cfa6b6f2
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2024-01-03 11:09:52 +0530

    🔨 Update yt video details ingestion

--- frontend/youtube/video_details.py
+++ frontend/youtube/video_details.py
@@ -1,43 +1,70 @@
 import re
 
 import polars as pl
+import polars.selectors as cs
 
-from frontend.configs import (
-    CATEGORY_ID_MAP_PATH,
-    INGESTED_YT_HISTORY_DATA_PATH,
-    VIDEO_DETAILS_JSON_PATH,
-)
+from frontend.configs import INGESTED_YT_HISTORY_DATA_PATH, VIDEO_DETAILS_JSON_PATH
+
+CATEGORY_ID_MAP = {
+    "1": "Film & Animation",
+    "2": "Autos & Vehicles",
+    "10": "Music",
+    "15": "Pets & Animals",
+    "17": "Sports",
+    "18": "Short Movies",
+    "19": "Travel & Events",
+    "20": "Gaming",
+    "21": "Videoblogging",
+    "22": "People & Blogs",
+    "23": "Comedy",
+    "24": "Entertainment",
+    "25": "News & Politics",
+    "26": "Howto & Style",
+    "27": "Education",
+    "28": "Science & Technology",
+    "29": "Nonprofits & Activism",
+    "30": "Movies",
+    "31": "Anime/Animation",
+    "32": "Action/Adventure",
+    "33": "Classics",
+    "34": "Comedy",
+    "35": "Documentary",
+    "36": "Drama",
+    "37": "Family",
+    "38": "Foreign",
+    "39": "Horror",
+    "40": "Sci-Fi/Fantasy",
+    "41": "Thriller",
+    "42": "Shorts",
+    "43": "Shows",
+    "44": "Trailers",
+}
 
 
 class VideoDetails:
-    def __init__(self) -> None:
-        __df = pl.read_json(INGESTED_YT_HISTORY_DATA_PATH)
-        __vdf = pl.read_json(VIDEO_DETAILS_JSON_PATH)
-        __cat_id_df = pl.read_json(CATEGORY_ID_MAP_PATH).transpose(
+    def __init__(self, path: str | None = None) -> None:
+        ingested_hist_df = pl.read_json(INGESTED_YT_HISTORY_DATA_PATH)
+        video_details_df = pl.read_json(path if path else VIDEO_DETAILS_JSON_PATH)
+        category_id_df = pl.DataFrame._from_dict(CATEGORY_ID_MAP).transpose(
             include_header=True,
             header_name="categoryId",
             column_names=["categoryName"],
         )
-        self.df = __df.join(
-            __vdf.drop("title", "channelTitle", "channelId"),
-            left_on="videoId",
-            right_on="id",
-        ).join(__cat_id_df, on="categoryId")
+        self.df = ingested_hist_df.join(
+            video_details_df, left_on="videoId", right_on="id"
+        ).join(category_id_df, on="categoryId")
 
     def __handle_duration(self, x: str):
         total_sec = 0
-        # Hours
-        hr = re.search(r"(\d+)H", x)
+        hr = re.search(r"(\d+)H", x)  # Hours
         total_sec += int(hr.group(1)) * 3600 if hr else 0
-        # Minutes
-        min = re.search(r"(\d+)M", x)
+        min = re.search(r"(\d+)M", x)  # Minutes
         total_sec += int(min.group(1)) * 60 if min else 0
-        # Seconds
-        sec = re.search(r"(\d+)S", x)
+        sec = re.search(r"(\d+)S", x)  # Seconds
         total_sec += int(sec.group(1)) if sec else 0
         return total_sec
 
-    def __data_cleaning(self, df: pl.DataFrame) -> pl.DataFrame:
+    def _data_cleaning(self, df: pl.DataFrame) -> pl.DataFrame:
         df = df.with_columns(
             pl.col("publishedAt").str.to_datetime(),
             pl.col("duration")
@@ -49,17 +76,19 @@ class VideoDetails:
             .add(pl.col("isShorts"))
             .cast(bool)
             .alias("isShorts"),
-            pl.col("hour")
-            .cut(
-                breaks=[5, 12, 17, 21],
-                labels=["Night", "Morning", "Afternoon", "Evening", "Night"],
-                left_closed=True,
-            )
-            .alias("daytime"),
-        ).drop("duration", "categoryId")
+        )
 
         return df
 
+    def _drop_cols(self, df: pl.DataFrame) -> pl.DataFrame:
+        drop_cols = [
+            "duration",
+            "categoryId",
+            cs.ends_with("_right"),
+        ]
+        return df.drop(drop_cols)
+
     def initiate(self) -> pl.DataFrame:
-        df = self.__data_cleaning(self.df)
+        df = self._data_cleaning(self.df)
+        df = self._drop_cols(df)
         return df
