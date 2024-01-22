778a0ffc9e48fc43831a3b770f29368e687f4ba2
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2024-01-22 10:53:45 +0530

    🔨 Update backend/api_utils.py

--- backend/api_utils.py
+++ backend/api_utils.py
@@ -1,53 +1,15 @@
 import logging
-from typing import overload
 
 import httpx
 
 from api.configs import API_HOST_URL
 
 
-@overload
 def make_request_to_api(
     *,
     method: str,
     endpoint: str,
     timeout: int = 5,
-    return_type: type[str] = str,
-    **kwargs_for_request,
-) -> str:
-    ...
-
-
-@overload
-def make_request_to_api(
-    *,
-    method: str,
-    endpoint: str,
-    timeout: int = 5,
-    return_type: type[bytes] = bytes,
-    **kwargs_for_request,
-) -> bytes:
-    ...
-
-
-@overload
-def make_request_to_api(
-    *,
-    method: str,
-    endpoint: str,
-    timeout: int = 5,
-    return_type: type[dict] = dict,
-    **kwargs_for_request,
-) -> dict:
-    ...
-
-
-def make_request_to_api(
-    *,
-    method: str,
-    endpoint: str,
-    timeout: int = 5,
-    return_type: type[str | bytes | dict] = str,
     **kwargs_for_request,
 ) -> str | bytes | dict:
     try:
@@ -68,11 +30,4 @@ def make_request_to_api(
         logging.error(error_msg)
         raise httpx.HTTPStatusError(error_msg, request=res.request, response=res)
 
-    if return_type is str:
-        return res.text
-    elif return_type is bytes:
-        return res.content
-    elif return_type is dict:
-        return res.json()
-    else:
-        raise ValueError(f"{return_type = }")
+    return res.content
