c7f7e8b0ffd425d98ebb93ae566b07d8925d061f
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2024-02-01 11:23:25 +0530

    🔥 Remove `backend/ml/`

--- backend/ml/channel_reco/configs.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-CH_RECO_DATA_PATH = Path("data/channel_reco/training.json")
-CH_RECO_TRANSFORMER_DATA_PATH = Path(
-    "data/channel_reco/ml_models/transformer_data.parquet"
-)
-CH_RECO_TRANSFORMER_PATH = Path("data/channel_reco/ml_models/transformer.dill")
